d58b12bee7ea489213086d8a1a7d9222d87e438a
KANTİN <KANTN@PCKASA>
2017-08-08 10:34:38 +0300

    PROJE EKLENDİ

---
+++ ZeonTicaret/ZeonTicaret.WebUI/App_Classes/Settings.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+
+namespace ZeonTicaret.WebUI.App_Classes
+{
+    public static class Settings
+    {
+        public static Size UrunOrtaBoyut
+        {
+            get
+            {
+                Size sz = new Size();
+                sz.Width =Convert.ToInt32(ConfigurationManager.AppSettings["UrunOrtaWidth"]);
+                sz.Height = Convert.ToInt32(ConfigurationManager.AppSettings["urunOrtaHeight"]);
+                return sz;
+            }
+        }
+        public static Size UrunBuyukBoyut
+        {
+            get
+            {
+                Size sz = new Size();
+                sz.Width = Convert.ToInt32(ConfigurationManager.AppSettings["UrunBuyukWidth"]);
+                sz.Height = Convert.ToInt32(ConfigurationManager.AppSettings["urunBuyukHeight"]);
+                return sz;
+            }
+        }
+    }
+}
